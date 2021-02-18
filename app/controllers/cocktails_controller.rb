c359e555d319f44ade46ca5a57bce3db3abc0456
Landon Lapensee <landonlapensee@gmail.com>
2021-02-18 13:49:14 -0800

    Added views for and controller for doses

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -5,6 +5,7 @@ class CocktailsController < ApplicationController
 
   def show
     @cocktail = Cocktail.find(params[:id])
+    @ingredients = Ingredient.all
   end 
 
   def new
@@ -12,10 +13,10 @@ class CocktailsController < ApplicationController
   end
   
   def create
-    @cocktail = Cocktail.new(cocktail_params)
-    @cocktail.save
+    cocktail = Cocktail.new(cocktail_params)
+    cocktail.save
 
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktails_path
   end
 
   private
